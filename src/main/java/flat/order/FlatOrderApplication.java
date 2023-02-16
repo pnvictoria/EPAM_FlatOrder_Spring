6dd1014ef34f085418775c10ff15069598a09bb1
Viktoriya Ponomarenko <pnvictoria13.work@gmail.com>
2023-02-16 23:58:34 +0200

    Initial commit

---
+++ src/main/java/flat/order/FlatOrderApplication.java
@@ -0,0 +1,13 @@
+package flat.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flat/order/FlatOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package flat.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
