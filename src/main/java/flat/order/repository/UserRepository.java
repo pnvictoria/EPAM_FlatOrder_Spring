584617924e8eb8c38c706b41275be46be17c1c62
Viktoriya Ponomarenko <pnvictoria13.work@gmail.com>
2023-02-22 03:17:10 +0200

    Fix test UserRepository

--- src/main/java/flat/order/repository/UserRepository.java
+++ src/main/java/flat/order/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package flat.order.repository;public interface UserRepository {
+package flat.order.repository;
+
+import flat.order.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
